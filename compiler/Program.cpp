9690488b3d67689d40a3017b037a45a85e1daaf1
fchaubeau <f.chaubeau@gmail.com>
2021-03-30 11:43:22 +0200

    added cfg and bb

---
+++ compiler/Program.cpp
@@ -0,0 +1,29 @@
+#include "IR.h"
+
+using namespace std;
+
+Program::Program()
+{
+	this->cfgs = vector<CFG*>();
+}
+
+void Program::add_cfg(CFG* cfg)
+{
+    this->cfgs.push_back(cfg);
+}
+
+void Program::gen_asm(ostream & o, int offsetReturn)
+{
+    for(int i = 0; i < this->cfgs.size(); i++)
+    {
+        this->cfgs[i]->gen_asm_prologue(o);
+        this->cfgs[i]->gen_asm(o);
+        this->cfgs[i]->gen_asm_epilogue(o, offsetReturn);
+    }
+
+}
+
+CFG* Program::get_cfg_by_index(int index)
+{
+    return this->cfgs.at(index);
+}
