57551b669165e113d0cd3c734753f9cf8665d4b4
PierreDuj <pdujardin50@hotmail.com>
2021-04-02 08:20:59 +0200

    correction test 13.1

--- tests/tests/Init/13-1_multi_type.c
+++ tests/tests/Init/13-1_multi_type.c
@@ -1,12 +1,8 @@
 #include <stdint.h>
 
 int main() {
-    int a,b;
-    char c,d;
-    int64_t e,f;
-    a = 3;
-    c = 'c';
-    d = 135;
-    e = 75;
+    int a,b = 3;
+    char c,d = 'c', e = 135;
+    int64_t f,g = 75;
     return 0;
 }
