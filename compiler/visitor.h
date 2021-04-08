f48a98e81a40df94718c5be2e9561c70df8de243
PierreDuj <pdujardin50@hotmail.com>
2021-04-08 21:39:01 +0200

    comment visitor elsebloc

--- compiler/visitor.h
+++ compiler/visitor.h
@@ -325,14 +325,14 @@ public:
     return 0;
   }
   
-  virtual antlrcpp::Any visitElseBloc(ifccParser::ElseBlocContext *ctx) override {
+ /* virtual antlrcpp::Any visitElseBloc(ifccParser::ElseBlocContext *ctx) override {
     visit(ctx->bloc());
 
     Write_label* write_labelInstr = new Write_label(".L2", this->program->get_cfg_by_index(0)->get_bb_by_index(0));
     IRInstr* instr = dynamic_cast<IRInstr*> (write_labelInstr);
     this->program->get_cfg_by_index(0)->get_bb_by_index(0)->add_IRInstr(instr);
     return 0;
-  }
+  }*/
 
 
   /**
