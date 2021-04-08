86446d4d7650b318f07ad62ae7b614ddc19cd681
PierreDuj <pdujardin50@hotmail.com>
2021-04-08 22:38:57 +0200

    2 tests de +

---
+++ tests/tests/Init/6-if/6-14_double_ifelse.c
@@ -0,0 +1,16 @@
+int main() {
+    int a = 2,b;
+    if(a > 3) {
+        b = 1;
+    }
+    else{
+        b = 3;
+    }
+    if(b < 2) {
+        a = 8;
+    }
+    else {
+        a = 7;
+    }
+    return a;
+}
---
+++ tests/tests/Init/6-if/6-15_empty_else.c
@@ -0,0 +1,9 @@
+int main() {
+    int a = 2,b;
+    if(a > 3) {
+        b = 1;
+    }
+    else {
+    }
+    return b;
+}
