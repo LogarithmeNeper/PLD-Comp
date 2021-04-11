fad11e177cbe5187a24bdc3f74f4601227e5c479
LoLaSiDo <cremer.lola@gmail.com>
2021-04-11 16:46:04 +0200

    nouveaux tests if

---
+++ tests/tests/Init/6-if/6-19_three_if.c
@@ -0,0 +1,13 @@
+int main() {
+    int a = 7, b = 1;
+    if (a > 2) {
+        b = 2;
+        if (a > 4) {
+            b = 3;
+            if (a > 6) {
+                b = 4;
+            }
+        }
+    }
+    return b;
+}
