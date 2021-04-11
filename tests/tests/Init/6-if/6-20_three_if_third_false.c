96fa6894d5e2c2b19de37f296f0393b55ed107fe
PierreDuj <pdujardin50@hotmail.com>
2021-04-11 16:59:00 +0200

    fail 6-20

--- tests/tests/Init/6-if/6-20_three_if_third_false.c
+++ tests/tests/Init/6-if/6-20_three_if_third_false.c
@@ -2,7 +2,7 @@ int main() {
     int a = 7, b = 1;
     if (a > 2) {
         b = 2;
-        if (a > 4) {
+        if (a < 4) {
             b = 3;
             if (a < 6) {
                 b = 4;
