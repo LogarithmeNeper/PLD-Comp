98bc43f55c962b404a336d15b65ade7acd3bae2b
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-04-11 16:17:59 +0200

    documentation write label

--- compiler/write_label.h
+++ compiler/write_label.h
@@ -1,18 +1,22 @@
 #pragma once
 
+// Personal includes
 #include "IR.h"
 #include "basic_block.h"
+// System include
 #include <iostream>
 
-class Write_label : public IRInstr{ // Used to load a constant into a temporary var
-    public:
-        Write_label();
-        Write_label(const string & _destinationLabel, BasicBlock* bb);
-        void gen_asm(ostream & o = cout);
-        ~Write_label();
-        string destinationLabel; // the label of the part of code inside the if statement
+// Class used to write to a label as an instruction
+class Write_label : public IRInstr
+{
+    // Public methods and attributes
+public:
+    Write_label();
+    Write_label(const string &_destinationLabel, BasicBlock *bb);
+    void gen_asm(ostream &o = cout);
+    ~Write_label();
+    string destinationLabel; // the label of the part of code inside the if statement
 
-    protected:
-        
-
+    // Protected methods and attributes
+protected:
 };
