99a10e4b964b187215a9efd68468ae986d10eecb
LoLaSiDo <cremer.lola@gmail.com>
2021-04-11 18:03:29 +0200

    tests sur les fonctions

---
+++ tests/tests/Init/5-Fonctions/5-16_comma_between_parameters_in_call.c
@@ -0,0 +1,9 @@
+int myFunction(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int a = 7, b = 2;
+    int c = myFunction(a b);
+    return c;
+}
---
+++ tests/tests/Init/5-Fonctions/5-17_functions_with_same_name.c
@@ -0,0 +1,13 @@
+int myFunction(int a) {
+    return a + 2;
+}
+
+int myFunction(int a) {
+    return a + 3;
+}
+
+int main() {
+    int a = 1, b;
+    b = myFunction(a);
+    return b;
+}
