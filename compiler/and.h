a7b2c4dd97f2feb9730b98ffd27d6a453e6cf421
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-04-11 15:30:46 +0200

    documentation and

--- compiler/and.h
+++ compiler/and.h
@@ -1,19 +1,25 @@
 #pragma once
 
+// Personal include
 #include "IR.h"
+
+// System include
 #include <iostream>
 
-class And : public IRInstr{ // Used to make an addition
-    public:
-        And();
-        And(const int & _offsetLeft, const int & _offsetRight, const int & _offsetDest, BasicBlock* bb);
-        void gen_asm(ostream & o = cout);
-        ~And();
-        int offsetLeft; // the offset of the left var
-        int offsetRight; // the offset of the right var
-        int offsetDest; // the offset of the destination var
+// Class to make an logival and instruction
+// Inherit from IRInstr
+class And : public IRInstr
+{
+    // Public methods and attributes
+public:
+    And();
+    And(const int &_offsetLeft, const int &_offsetRight, const int &_offsetDest, BasicBlock *bb);
+    void gen_asm(ostream &o = cout);
+    ~And();
+    int offsetLeft;  // the offset of the left var
+    int offsetRight; // the offset of the right var
+    int offsetDest;  // the offset of the destination var
 
-    protected:
-        
-
+    // Protected methods and attributes
+protected:
 };
