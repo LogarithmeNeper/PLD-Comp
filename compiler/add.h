83fef68ffa78874cab335e7f50918dc55bf969bf
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-04-11 15:29:00 +0200

    fix

--- compiler/add.h
+++ compiler/add.h
@@ -5,24 +5,23 @@
 // System includes
 #include <iostream>
 
-// Personal includes 
+// Personal includes
 #include "IR.h"
 
 // Used to make an addition instruction
 // Inherits from Instruction
-class Add : public IRInstr { 
+class Add : public IRInstr
+{
     // Public methods and attributes
-    public:
-        Add();
-        Add(const int & _offsetLeft, const int & _offsetRight, const int & _offsetDest, BasicBlock* bb);
-        void gen_asm(ostream & o = cout);
-        ~Add();
-        int offsetLeft; // the offset of the left var
-        int offsetRight; // the offset of the right var
-        int offsetDest; // the offset of the destination var
+public:
+    Add();
+    Add(const int &_offsetLeft, const int &_offsetRight, const int &_offsetDest, BasicBlock *bb);
+    void gen_asm(ostream &o = cout);
+    ~Add();
+    int offsetLeft;  // the offset of the left var
+    int offsetRight; // the offset of the right var
+    int offsetDest;  // the offset of the destination var
 
     // Protected methods and attributes
-    protected:
-        
-
+protected:
 };
